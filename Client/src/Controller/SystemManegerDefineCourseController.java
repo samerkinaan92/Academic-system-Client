a52a49369bbf82d27a29dea1eea419c2b07c4313
Idan <Idan@Idan-PC>
2017-05-27 21:12:35 +0300

    Using Course Entity.

--- Client/src/Controller/SystemManegerDefineCourseController.java
+++ Client/src/Controller/SystemManegerDefineCourseController.java
@@ -4,13 +4,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import javax.swing.JOptionPane;
 
+import Entity.Course;
 import application.Main;
-import application.MessageThread;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -24,7 +23,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class SystemManegerDefineCourseController implements Initializable {
-	
 	
 	@FXML
     private Label nameErr;
@@ -59,23 +57,26 @@ public class SystemManegerDefineCourseController implements Initializable {
     @FXML
     private TextField courseName;
     
-    String[] hours = {"1", "2", "3", "4"};
-    String[] units = {"Software", "Mathematics", "Physics", "English"};
+    //-------------------------------------------------------------------------------------------------------------------
     
-   
+    private final String[] hours = {"1", "2", "3", "4"};
+    private final String[] units = {"Software", "Mathematics", "Physics", "English"};
     
-    ArrayList<String> result;
+    ArrayList<Course> courseArr;
+    
     ArrayList<String> organizedList;
     ArrayList<String> PreList = new ArrayList<String>();
+    
+    Course newCourse;
+    boolean err;
     
     private final ObservableList<String> boxUnit = FXCollections.observableArrayList(units);
     private final ObservableList<String> boxHours = FXCollections.observableArrayList(hours);
     private ObservableList<String> coursesDB;
     
-    private HashMap <String, String> msgServer;
+    //-------------------------------------------------------------------------------------------------------------------
     
-    
-    public void search(ActionEvent e){
+    public void search(ActionEvent e){ // Filter course list.
     	ArrayList<String> temp = new  ArrayList<String>();
     	
     	for (int i = 0; i < organizedList.size(); i++)
@@ -86,81 +87,61 @@ public class SystemManegerDefineCourseController implements Initializable {
 		courseList.setItems(coursesDB);
     }
     
-    public void addPreCourse(ActionEvent e){
+    private ArrayList<String> getList(ArrayList<Course> courseArr){ // Get courses names & ID's for list.
     	
-    	for (int i = 0; i < PreList.size(); i++)
-    		if (courseList.getSelectionModel().getSelectedItem().equals(PreList.get(i)))
-    			return;
-    	
-    	PreList.add(courseList.getSelectionModel().getSelectedItem());
-    	Collections.sort(PreList);
-    	preCourseList.setItems(FXCollections.observableArrayList(PreList));
-    }
-
-    public void removePreCourse(ActionEvent e){
-	
-    	for (int i = 0; i < PreList.size(); i++)
-    		if (preCourseList.getSelectionModel().getSelectedItem().equals(PreList.get(i)))
-    			PreList.remove(i);
-    	
-    	Collections.sort(PreList);
-    	preCourseList.setItems(FXCollections.observableArrayList(PreList));	
-    }
+		ArrayList<String> temp = new ArrayList<String>();
+		for (int i = 0; i < courseArr.size(); i++)
+			temp.add(courseArr.get(i).getName() + " (" + courseArr.get(i).getCourseID() + ")");
+		
+		return temp;	
+	}
     
-    public void submitCourse(ActionEvent e) throws IOException{
-    	
-    	boolean err = false;
-    	
-    	// Reset Screen errors.
-    	
-    	idErr.setText("");
-    	nameErr.setText("");
-    	hoursErr.setText("");
-    	unitErr.setText("");
-    	
-    	//--------------------------------------------------------------------------------------------------
-    	
-    	// Check for illegal input. //
-    	
-    	try{
-    		Integer.parseInt(courseId.getText());
-    		if (courseId.getText().isEmpty())
-    			throw new Exception();
-    	}
-    	catch(Exception exp1){
-    		idErr.setText("Course Id not valid!");
-    		err = true;
+    public void submitCourse(ActionEvent e) throws IOException{ //  Submit course for check & insert.
+    	  	
+    	if (err){ // Reset Screen errors.
+	    	idErr.setText("");
+	    	nameErr.setText("");
+	    	hoursErr.setText("");
+	    	unitErr.setText("");
     	}
     	
-    	if (courseName.getText().isEmpty()){
-    		nameErr.setText("Course Name not valid!");
-    		err = true;
-    	}
-    	
-    	if (weaklyHours.getSelectionModel().getSelectedItem() == null){
-    		hoursErr.setText("Weakly Hours not selected!");
-    		err = true;
-    	}
-    	
-    	if (teachingUnit.getSelectionModel().getSelectedItem() == null){
-    		unitErr.setText("Teaching Unit not selected!");
-    		err = true;
-    	}
-    	
-    	if (err)
+    	if (checkIlegalInput())
     		return;
-    	//---------------------------------------------------------------------------------------------------------
     	
+    	if (!Course.checkLegal(courseId.getText())){
+    		idErr.setText("Course ID already exists!");
+    		return;
+    	}
+    	
+    	newCourse = new Course(Integer.parseInt(courseId.getText()), courseName.getText(),
+    			teachingUnit.getSelectionModel().getSelectedItem().toString(), 
+    			Integer.parseInt(weaklyHours.getSelectionModel().getSelectedItem().toString()));
+    	
+    	newCourse.insertCourse();
+    	newCourse.updatePreCourses(PreList);
+    	
+    	int numOfChanges = (int)Main.client.getMessage();
+    	
+    	if (numOfChanges >= 1){
+	    	JOptionPane.showMessageDialog(null,"Course definition successful :)");
+	    	clearScreen();
+    	}
+    	else
+    		JOptionPane.showMessageDialog(null, 
+					  "No changes were made!", "Error", JOptionPane.ERROR_MESSAGE);
+    	
+    	//---------------------------------------------------------------------------------------------------------
+    	/*
     	// Verify course id in data base.
     	
     	if (checkId(courseId.getText())){
     		idErr.setText("Course ID already exists!");
     		return;
     	}
-    	
+    	*/
     	//--------------------------------------------------------------------------------------------------------------
     	
-    	
+    	/*
     	// Update course in data base. 
     	
     	String msg = "Insert INTO course (CourseID, Name, TUID, weeklyHours)";
@@ -186,11 +167,11 @@ public class SystemManegerDefineCourseController implements Initializable {
 			exp.printStackTrace();
 		}}
     	
-		
-    	int numOfChanges = (int)Main.client.getMessage();
+		*/
+    	
     	
     	//------------------------------------------------------------------------------------------------------------
-    	
+    	/*
     	
     	// Update Precourses in data base.
     	
@@ -219,81 +200,57 @@ public class SystemManegerDefineCourseController implements Initializable {
 				exp.printStackTrace();
 			}}
     	}
-    	
+    	*/
     	//--------------------------------------------------------------------------------------------------------------
     	
-    	numOfChanges += (int)Main.client.getMessage();
+    //	numOfChanges += (int)Main.client.getMessage();
     	
-    	if (numOfChanges >= 1){
-	    	JOptionPane.showMessageDialog(null,"Course definition successful :)");
-	    	clearScreen();
-    	}
-    	else
-    		JOptionPane.showMessageDialog(null, 
-					  "No changes were made!", "Error", JOptionPane.ERROR_MESSAGE);
+    	
     }
-    
-
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1){
+	
+	private boolean checkIlegalInput(){ // Check for illegal input.
+		err = false;
 		
-		loadDB();
-		
-		weaklyHours.setItems(boxHours);
-		teachingUnit.setItems(boxUnit);
-		
+		try{
+    		Integer.parseInt(courseId.getText());
+    		if (courseId.getText().isEmpty())
+    			throw new Exception();
+    	}
+    	catch(Exception exp1){
+    		idErr.setText("Course Id not valid!");
+    		err = true;
+    	}
+    	
+    	if (courseName.getText().isEmpty()){
+    		nameErr.setText("Course Name not valid!");
+    		err = true;
+    	}
+    	
+    	if (weaklyHours.getSelectionModel().getSelectedItem() == null){
+    		hoursErr.setText("Weakly Hours not selected!");
+    		err = true;
+    	}
+    	
+    	if (teachingUnit.getSelectionModel().getSelectedItem() == null){
+    		unitErr.setText("Teaching Unit not selected!");
+    		err = true;
+    	}
+    	
+    	return err;
 	}
 	
-	private void clearScreen() throws IOException{
+	private void clearScreen() throws IOException{ // Clear & update screen details.
 		AnchorPane pane = FXMLLoader.load(getClass().getResource("/FXML/SystemManagerDefineCourses.fxml"));;
 		Main.getRoot().setCenter(pane);
 	}
 	
-	@SuppressWarnings("unchecked")
-	private void loadDB(){
-		
-		msgServer = new HashMap <String,String>();
-		msgServer.put("msgType", "select");
-		msgServer.put("query", "Select Name,CourseID From course");
-		
-		try{
-			Main.client.sendMessageToServer(msgServer);
-			}
-			catch(Exception exp){
-				System.out.println("Server fatal error!");
-			}
-		MessageThread msgT = new MessageThread(Main.client);
-		msgT.start();
-		synchronized (msgT){try {
-			msgT.wait();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}}
-		
-		result = (ArrayList<String>)Main.client.getMessage();
-		organizedList = getList(result);
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1){ // Initialize window.
+		courseArr = Course.getCourses();
+		organizedList = getList(courseArr);
 		Collections.sort(organizedList);
-		coursesDB  = FXCollections.observableArrayList(organizedList);
-		
-		courseList.setItems(coursesDB);
+		courseList.setItems(FXCollections.observableArrayList(organizedList));
+		weaklyHours.setItems(boxHours);
+		teachingUnit.setItems(boxUnit);	
 	}
-	
-	private ArrayList<String> getList(ArrayList<String> list){
-		ArrayList<String> temp = new ArrayList<String>();
-		for (int i = 0; i < list.size(); i+=2)
-			temp.add(list.get(i) + " (" + list.get(i+1) + ")");
-		
-		return temp;	
-	}
-	
-	private boolean checkId(String id){
-		
-		for (int i = 1; i < result.size(); i+=2)
-			if (result.get(i).equals(id))
-				return true;
-		return false;
-		
-	}
-	
-	
 }
