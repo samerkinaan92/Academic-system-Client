254e85b60a51614a10f5850e3a04b5c747748388
tallin900 <tallin900@gmail.com>
2017-06-08 00:56:10 +0300

    Adding PrincipalDeletePermissionsController

--- Client/src/Controller/PrincipalMenuBarController.java
+++ Client/src/Controller/PrincipalMenuBarController.java
@@ -1,13 +1,24 @@
 package Controller;
 
 import java.io.IOException;
+import java.net.URL;
 
 import javax.swing.JOptionPane;
 
 import application.Main;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
@@ -87,6 +98,22 @@ public class PrincipalMenuBarController {
 		
 		
 	}
+	
+
+    @FXML
+    void RemovePerEvent(ActionEvent event) throws IOException {
+    	 	
+ 
+    	try {
+		   URL paneOneUrl = getClass().getResource("/FXML/PrincipalDeletePermissions.fxml");
+		   AnchorPane paneOne = FXMLLoader.load( paneOneUrl );
+		   BorderPane border = Main.getRoot();			    
+		   border.setCenter(paneOne);
+        } catch (IOException e) {
+            e.printStackTrace();
+          }        
+    }
+    
 	
 	/** Private Actions */
 
