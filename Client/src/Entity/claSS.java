517c95d475c693c4228c49471d24a3c57cfff10f
Aramis13 <idanagam87@gmail.com>
2017-06-24 21:20:06 +0300

    .

--- Client/src/Entity/claSS.java
+++ Client/src/Entity/claSS.java
@@ -112,6 +112,29 @@ public class claSS {
 		return DBstudent;
 	}
 	
+	@SuppressWarnings("unchecked")
+	public static ArrayList<String> getClassesOfCourse(String CourseId, String SemesterID){
+		
+		HashMap <String,String> msgServer = new HashMap <String,String>();
+		msgServer.put("msgType", "select");
+		msgServer.put("query", "Select ClassName From class_course where class_course.CourseID = '"+CourseId+"' and semesterId = '"+SemesterID+"';");
+		
+		try{
+			Main.client.sendMessageToServer(msgServer);
+			}
+			catch(Exception exp){
+				System.out.println("Server fatal error!");
+			}
+		synchronized (Main.client){try {
+			Main.client.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}}
+		ArrayList<String> result = (ArrayList<String>)Main.client.getMessage();
+		return result;
+		
+	}
+	
 	/**
 	 * Remove courses from a single class
 	 * @param cLass the class the courses will be removed from
