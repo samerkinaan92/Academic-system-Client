1e0f7989b70d933ae41e376b2cd6be88de79f0de
Aramis13 <idanagam87@gmail.com>
2017-06-24 21:19:12 +0300

    .

--- Client/src/Entity/Course.java
+++ Client/src/Entity/Course.java
@@ -153,6 +153,29 @@ public class Course extends AcademicActivity {
 		return DBcourses;
 	}
 	
+	@SuppressWarnings("unchecked")
+	public static ArrayList<String> getPreCoursesId(String ID){ // Get list of courses.
+		HashMap <String,String> msgServer = new HashMap <String,String>();
+		msgServer.put("msgType", "select");
+		msgServer.put("query", "Select preCourseID From pre_courses Where CourseID = '"+ID+"';");
+		
+		try{
+			Main.client.sendMessageToServer(msgServer);
+			}
+			catch(Exception exp){
+				System.out.println("Server fatal error!");
+			}
+		synchronized (Main.client){try {
+			Main.client.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}}
+		ArrayList<String> result = (ArrayList<String>)Main.client.getMessage();
+		
+		return result;
+	}
+	
+	
 	/**
 	 * Get all pre courses for a specific course
 	 * @param cID Course id to check all pre courses
