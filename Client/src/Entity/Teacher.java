4d8cea14bba5c3a871985875aeed35f093e121ec
Aramis13 <idanagam87@gmail.com>
2017-06-24 21:39:28 +0300

    .

--- Client/src/Entity/Teacher.java
+++ Client/src/Entity/Teacher.java
@@ -180,6 +180,59 @@ public class Teacher extends User {
 		return DBteachers;
 	}
 	
+	@SuppressWarnings("unchecked")
+	public static ArrayList<Teacher> getTeachersInSemester(String semesterID){
+		HashMap <String,String> msgServer = new HashMap <String,String>();
+		msgServer.put("msgType", "select");
+		msgServer.put("query", " Select distinct users.Name, users.ID, users.phoneNum, users.email, users.address From users,class_course WHERE users.ID = class_course.teacherID AND class_course.semesterId = '"+semesterID+"';");
+		//, phoneNum, email, address
+		try{
+			Main.client.sendMessageToServer(msgServer);
+			}
+			catch(Exception exp){
+				System.out.println("Server fatal error!");
+			}
+		synchronized (Main.client){try {
+			Main.client.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		}
+		ArrayList<String> result = (ArrayList<String>)Main.client.getMessage();
+		msgServer = new HashMap <String,String>();
+		msgServer.put("msgType", "select");
+		msgServer.put("query", "Select distinct teacher.TeacherID, teacher.MaxWorkHours From class_course, teacher, users WHERE teacher.TeacherID = class_course.teacherID AND class_course.semesterId = '"+semesterID+"';");
+		//Select distinct teacher.TeacherID, teacher.MaxWorkHours From class_course, teacher, users WHERE users.ID = class_course.teacherID AND class_course.semesterId = '"+semesterID+"';
+		try{
+			Main.client.sendMessageToServer(msgServer);
+			}
+			catch(Exception exp){
+				System.out.println("Server fatal error!");
+			}
+		synchronized (Main.client){try {
+			Main.client.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}}
+		ArrayList<String> result2 = (ArrayList<String>)Main.client.getMessage();
+		
+		for (int i = 1; i < result.size(); i+=5)//2
+			for (int j = 0; j < result2.size(); j+=2)//2
+				if (result.get(i).equals(result2.get(j))){
+					result.add(i+4, result2.get(j+1));
+					i++;
+					break;
+				}
+		
+		ArrayList<Teacher> DBteachers = new ArrayList<Teacher>();
+		
+		for (int i = 0; i < result.size(); i+=6)//3
+			DBteachers.add(new Teacher(result.get(i), result.get(i+1), result.get(i+2) ,result.get(i+3) ,result.get(i+4) ,Integer.parseInt(result.get(i+5))));
+		//, result.get(i+2) ,result.get(i+3) ,result.get(i+4) ,
+		return DBteachers;
+
+	}
+	
 	/*-------------------------------------  Get Sum of Hours  --------------------------------------*/
 	/**
 	 * Get sum of assigned working hours for teacher
