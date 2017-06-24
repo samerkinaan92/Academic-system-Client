d743a3617e26ec41aaa5b285236aae0e1330088a
Aramis13 <idanagam87@gmail.com>
2017-06-24 20:15:15 +0300

    ..

--- Client/src/Controller/LoginController.java
+++ Client/src/Controller/LoginController.java
@@ -1,21 +1,22 @@
 package Controller;
+
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
+import Entity.User;
 import application.ClientConnection;
 import application.Main;
-import application.MessageThread;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable {
 	
@@ -37,9 +38,11 @@ public class LoginController implements Initializable {
 	 @FXML
 	 private Label guiMeg;
 	 
-	 private final ObservableList<String> options = FXCollections.observableArrayList("MAT"); // List of schools.
+	 
+	 private final ObservableList<String> options = FXCollections.observableArrayList("Ort Braude", "Ort Kramim", "Majadim"); // List of schools.
 	 private HashMap <String, String> answer = null;
 	 private HashMap <String, String> msgServer = new HashMap <String,String>();
+	 
 	 
 	 @SuppressWarnings("unchecked")
 	 public void OnLogin(ActionEvent e) throws InterruptedException{
@@ -49,8 +52,8 @@ public class LoginController implements Initializable {
 		 try{
 			 Integer.parseInt(port.getText());
 		 }
-		 catch(Exception number){
-			 guiMeg.setText("Port/IP not valid!");
+		 catch(Exception ex){
+			 guiMeg.setText("Port field: input not valid!");
 			 return;
 		 }
 		
@@ -65,7 +68,7 @@ public class LoginController implements Initializable {
 		
 		// Send message to server with an new thread & wait for answer. 
 		
-		if (!id.getText().isEmpty() && !password.getText().isEmpty() && !school.getSelectionModel().getSelectedItem().toString().isEmpty()){
+		if (!id.getText().isEmpty() && !password.getText().isEmpty() && school.getSelectionModel().getSelectedItem() != null){
 			msgServer.put("msgType", "Login");
 			msgServer.put("id", id.getText());
 			msgServer.put("passwrd", password.getText());
@@ -76,11 +79,11 @@ public class LoginController implements Initializable {
 			}
 			catch(Exception exp){
 				guiMeg.setText("Server fatal error!");
+				return;
 			}
 			
-			MessageThread msgT = new MessageThread(Main.client);
-			msgT.start();
-			synchronized (msgT){msgT.wait();}
+			
+			synchronized (Main.client){Main.client.wait();}
 			answer = (HashMap <String, String>)Main.client.getMessage();
 		}
 		else if (Main.client != null){
@@ -90,12 +93,16 @@ public class LoginController implements Initializable {
 			
 		// Process answer from server.
 		
-		if (answer != null && answer.get("Valid").equals("true")){
+		if (answer != null && answer.get("Valid").equals("true")){	
 			
-			((Node)(e.getSource())).getScene().getWindow().hide(); // Close login window.
+			Stage Curstage = (Stage) id.getScene().getWindow();
+			Curstage.close();
+		    
+			//((Node)(e.getSource())).getScene().getWindow().hide(); // Close login window.
+			Main.user = new User(id.getText(), answer.get("Name"));
 			Main.openMain(answer.get("Type"));
-		}
-		else
+			Main.user.setType(answer.get("Type"));
+		}else
 			guiMeg.setText((String)answer.get("ErrMsg"));
 	 }
 
